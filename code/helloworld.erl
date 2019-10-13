074fb7c2702399aa3cca1e9f450dd8f1a183ace7
Sebastian Ganslandt <sebastian@ganslandt.nu>
2019-10-13 22:41:33 +0200

    Fixup test code

--- code/helloworld.erl
+++ code/helloworld.erl
@@ -1,6 +1,14 @@
 % hello world program
 -module(helloworld).
--export([start/0]).
+-export([start/0, foo/1, factorial/1]).
 
 start() ->
-    io:fwrite("Hello, world!\n").
+  io:fwrite("Hello, world!\n").
+
+foo(0) -> 1;
+foo(N) ->
+  1 + foo(N - 1).
+
+factorial(0) -> 1;
+factorial(N) ->
+  N * factorial(N - 1).
